ca3eb9a8dd6e0f0834e097bb1a402dbaab73d596
Turpal <salangeriev97@mail.ru>
2023-11-26 21:28:39 +0300

    the first project2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-let numberOFfilms = prompt("Сколько фильмов вы посмотрели.");
-let qiestionOne = prompt("Один из последних просмотренных фильмов");
-let qiestionTwo = prompt("Насколько оцените его");
-let qiestionThree = prompt("Один из последних просмотренных фильмов");
-let qiestionFour = prompt("Насколько оцените его");
-const personalMovieDB = {
-    count: numberOFfilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+"use strict"
+let num = 90;
+while(num >= 150) {
+    console.log(num);
+    num++;
 }
-personalMovieDB.movies[qiestionOne] = qiestionTwo;
-personalMovieDB.movies[qiestionThree] = qiestionFour;
-console.log(personalMovieDB);
